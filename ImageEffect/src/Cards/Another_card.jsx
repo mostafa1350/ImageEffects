aa7a13896576df856d4a5719d7d0f25ef0d8ac3e
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-22 21:09:47 +0330

    update "a" tag in another_page

--- ImageEffect/src/Cards/Another_card.jsx
+++ ImageEffect/src/Cards/Another_card.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 const Another_card = () => {
   return (
-    <div className='main_another flex flex-col'>
+    <div className='h-screen flex flex-col justify-center items-center bg-slate-800 m-0;'>
         <div>
         <h1>Another Page ...</h1>
         </div>
-        <div>
-            <a href="">
+        <div className='mt-[30px]'>
+            <a href="#" className='a_AnotherPage'>
                 <Link to="/">Home</Link>            
             </a>
         </div>
