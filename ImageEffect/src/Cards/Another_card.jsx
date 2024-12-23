19395ee31632d841a89ea4c266426185f0395354
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-23 23:03:28 +0330

    def new div for imageBox in another file

--- ImageEffect/src/Cards/Another_card.jsx
+++ ImageEffect/src/Cards/Another_card.jsx
@@ -3,14 +3,19 @@ import {Link} from 'react-router-dom';
 const Another_card = () => {
   return (
     <div className='h-screen flex flex-col justify-center items-center bg-slate-800 m-0;'>
-        <div>
-        <h1>Another Page ...</h1>
+      <div className='container'>
+        <div className='card'>
+          <div className='ImageBox'>
+            <img src="" alt="" />
+          </div>
+          <div className='contentBox'>
+            <h2></h2>
+            <div className='size'></div>
+            <div className='color'></div>
+            <a href=""></a>
+          </div>
         </div>
-        <div className='mt-[30px]'>
-            <a href="#" className='a_AnotherPage'>
-                <Link to="/">Home</Link>            
-            </a>
-        </div>
+      </div>
     </div>
   )
 }
